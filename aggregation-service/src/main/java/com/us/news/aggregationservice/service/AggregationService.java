03b8885fe123c26a4b329825f9b301b31146d827
olek <aleksander.mysko@gmail.com>
2024-09-18 12:21:05 +0200

    add client do AggregationService

--- aggregation-service/src/main/java/com/us/news/aggregationservice/service/AggregationService.java
+++ aggregation-service/src/main/java/com/us/news/aggregationservice/service/AggregationService.java
@@ -1,5 +1,6 @@
 package com.us.news.aggregationservice.service;
 
+import com.us.news.aggregationservice.client.ServiceClient;
 import com.us.news.aggregationservice.config.ServiceConfig;
 import com.us.news.aggregationservice.model.AggregationResultDto;
 import com.us.news.common.model.CreatedLocationDto;
@@ -7,29 +8,27 @@ import com.us.news.common.model.CreatedNewsDto;
 import com.us.news.common.model.ResponseWrapper;
 import lombok.AllArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClientException;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class AggregationService {
-    private final RestTemplate restTemplate;
+    private final ServiceClient serviceClient;
     private final ServiceConfig serviceConfig;
 
     public ResponseWrapper<AggregationResultDto> getLocationWithNews(UUID locationId) {
         try {
-            CreatedLocationDto location = fetchData(serviceConfig.getLocationServiceUrl() + locationId,
-                    new ParameterizedTypeReference<>() {});
+            CreatedLocationDto location = serviceClient.fetchData(serviceConfig.getLocationServiceUrl() + locationId,
+                    new ParameterizedTypeReference<ResponseWrapper<CreatedLocationDto>>() {
+                    }).getData();
 
-            List<CreatedNewsDto> newsList = fetchData( serviceConfig.getNewsServiceUrl() + locationId,
-                    new ParameterizedTypeReference<>() {});
-
+            List<CreatedNewsDto> newsList = serviceClient.fetchData(serviceConfig.getNewsServiceUrl() + locationId,
+                    new ParameterizedTypeReference<ResponseWrapper<List<CreatedNewsDto>>>() {
+                    }).getData();
 
             AggregationResultDto aggregationResult = AggregationResultDto.builder()
                     .location(location)
@@ -37,18 +36,12 @@ public class AggregationService {
                     .build();
 
             return new ResponseWrapper<>(true, "SUCCESS", aggregationResult);
+        } catch (NullPointerException e) {
+            return new ResponseWrapper<>(false, "Null value encountered: " + e.getMessage(), null);
+        } catch (RestClientException e) {
+            return new ResponseWrapper<>(false, "Error during service call: " + e.getMessage(), null);
         } catch (Exception e) {
-            return new ResponseWrapper<>(false, "Error during data aggregation: " + e.getMessage(), null);
+            return new ResponseWrapper<>(false, "Unexpected error: " + e.getMessage(), null);
         }
-    }
-
-    private <T> T fetchData(String url, ParameterizedTypeReference<ResponseWrapper<T>> responseType) {
-        ResponseEntity<ResponseWrapper<T>> response = restTemplate.exchange(
-                url,
-                HttpMethod.GET,
-                null,
-                responseType
-        );
-        return Objects.requireNonNull(response.getBody()).getData();
     }
 }
